8f500384423a93cc1d5b7111e644295c3b3b4bf7
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-06-11 09:34:59 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+
+  def initialize(first="", last="")
+    @first_name = first
+    @last_name = last
+    @@all << self
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
